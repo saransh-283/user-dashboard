1a08652e90eec570dc3811532320fae33fa4e6b8
saransh-283 <saranshmm@gmail.com>
2021-06-02 18:34:51 +0530

    Almost Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
 const navbar = document.getElementById('navbar').getBoundingClientRect()
 document.body.style.paddingTop = `${navbar.height}px`
+
+var tabs=Array.from(document.getElementsByClassName('tab'))
+
+function tabClick(e){
+	var elem=e.target
+	
+	document.getElementsByClassName('active-tab')[0].classList.remove('active-tab')
+	elem.classList.add('active-tab')
+	
+	var id=elem.getAttribute('data-tab-id')
+	document.getElementsByClassName('active')[0].classList.remove('active')
+	document.getElementById(id).classList.add('active')
+}
+
+tabs.map(elem=>elem.addEventListener('click',tabClick))
